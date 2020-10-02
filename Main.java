c2fbdad98d39177ce07d394e1f0a4c0f458c9465
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-02 13:12:26 +0000

    finished while loop example

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * While loop example
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +11,38 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+    Scanner input = new Scanner(System.in);
+  
+    // declare the variable
+    int upTo;
+    do{
+      System.out.println("What number to count to(positive integer)?");
+      upTo = input.nextInt();
+    }while(upTo <= 0);
+
+
+
+    // create a variable to keep track of the number
+    int count = 6;
+    // using a loop to count up to five
+    while(count <= upTo){
+      // print the number we're at 
+      System.out.println(count);
+      // increase the value of count by 1
+      count = count + 1;
+    } 
+    System.out.println("All done");
+  
+
+  // do while loop 
+  int count2 = 6;
+  // do while loop to count
+  do{
+    //print the number
+    System.out.println(count2);
+    // increment the counter
+    count2 = count2 + 1;
+  }while (count2 <= upTo);
+  System.out.println("All done again");
+}
 }
